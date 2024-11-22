219474df51c2a2238d34b2fe7067d572fd487caf
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-11-22 20:49:56 +0800

    chore: remove document of `struc_parse_wrapper()`

--- R/struc-parser-wrapper.R
+++ R/struc-parser-wrapper.R
@@ -1,24 +1,8 @@
-#' Wrapper for Structure Parsers
-#'
-#' @description
-#' This wrapper function adds the following functionalities to a
-#' structure parser function:
-#' - Check input type;
-#' - Vectorized parsing;
-#' - More informative error messages.
-#'
-#' This wrapper is used by all parser interfaces in the package,
-#' e.g. [parse_pglyco_struc()].
-#' If you are writing a new parser, you can use this function to
-#' wrap your parser function.
-#'
-#' @param x A character vector of structure strings.
-#' @param parser A function that parses a single structure string
-#' and returns a glycan graph (see [glyrepr::as_glycan_graph()]).
-#' @param mode A character string, either "ne" or "dn".
-#'
-#' @return A glycan graph if `x` is a single character,
-#' or a list of glycan graphs if `x` is a character vector.
+# This wrapper function adds the following functionalities to a
+# structure parser function:
+# - Check input type;
+# - Vectorized parsing;
+# - More informative error messages.
 struc_parser_wrapper <- function(x, parser, mode = "ne") {
   # Check input type
   checkmate::assert_character(x)
