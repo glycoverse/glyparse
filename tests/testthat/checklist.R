2adba93bfc6288767fa53a6620bf4aea3139d6c0
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-11-28 14:59:02 +0800

    tests: add a checklist for testing a parser

---
+++ tests/testthat/checklist.R
@@ -0,0 +1,29 @@
+# This file is a checklist for testing a parse.
+#
+# To thoroughly test a parser, the following should be tested:
+#
+# - Check if monosaccharide names are parsed correctly. Pay attention to
+#   special cases like "Neu5Ac", "Neu5Gc", "MurNGc", "Api", "LDmanHep",
+#   and "DDmanHep". Also make sure some monosaccharides are not parsed
+#   as substituents, e.g. "GlcNAc" should not be parsed as "Glc" and "2NAc".
+#
+# - Check if substituents are parsed correctly. This includes substituent
+#   names and positions. Positions can be integers or "?".
+#
+# - Check if anomers are parsed correctly. Anomers can be "a", "b", or "?".
+#
+# - Check if linkages are parsed correctly. Make sure unknown parts are
+#   handled correctly, e.g. "?1-4", "a1-?", "?1-?".
+#
+# - Check if the parser can handle glycans with only one monosaccaride.
+#
+# - For format using general representation of monosaccharides,
+#   e.g. GlycoCT, WURCS, etc., it is crucial to test all known monosaccharides.
+#
+# - Check if the parser can handle special cases where a monosaccharide name
+#   is split by a substituent, e.g. "Neu4Ac5Ac". For IUPAC series, this might
+#   not be an issue, because "Neup5Ac4Ac" is normally the correct way to
+#   arrange the substituents.
+#
+# - It is also advised to parse some complex glycans, and use `expect_snapshot()`
+#   to check if the parsed glycan is correct.
