bde0be90c1a4e77107de98019d9a824ff8ecb969
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-07-15 15:46:43 +0800

    feat: better error message
    Before:
    
    ```
    > parse_iupac_condensed("bad_glycan")
    Error in `purrr::map()` at glyparse/R/struc-parser-wrapper.R:13:3:
    ℹ In index: 1.
    Caused by error in `value[[3L]]()`:
    ! Could not parse IUPAC-condensed string: {.val {x}}
    ℹ Invalid characters or format in IUPAC-condensed string
    Run `rlang::last_trace()` to see where the error occurred.
    ```
    
    Now:
    
    ```
    > parse_iupac_condensed("bad_glycan")
    Error in `parse_iupac_condensed()`:
    ! Can't parse: "bad_glycan"
    Run `rlang::last_trace()` to see where the error occurred.
    ```

--- R/struc-parser-wrapper.R
+++ R/struc-parser-wrapper.R
@@ -3,15 +3,19 @@
 # - Check input type;
 # - Vectorized parsing;
 # - More informative error messages.
-struc_parser_wrapper <- function(x, parser) {
+struc_parser_wrapper <- function(x, parser, call = rlang::caller_env()) {
   checkmate::assert_character(x)
-  
+
   # Get unique values to avoid redundant computation
   unique_x <- unique(x)
-  
+
   # Parse only unique values
-  unique_graphs <- purrr::map(unique_x, parser)
-  
+  unique_graphs <- purrr::map(unique_x, purrr::possibly(parser, NA))
+  invalid_unique_x <- unique_x[is.na(unique_graphs)]
+  if (length(invalid_unique_x) > 0) {
+    cli::cli_abort("Can't parse: {.val {invalid_unique_x}}", call = call)
+  }
+
   # Map back to original dimension
   # Find the index of each original element in unique_x
   indices <- match(x, unique_x)
