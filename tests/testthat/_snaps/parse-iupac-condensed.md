47938bf810fb96031a3b27f4d4c477501eed99f7
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-07-30 17:57:34 +0800

    test: update test for `parse_iupac_condensed()`
    Add an anomer to the end.

--- tests/testthat/_snaps/parse-iupac-condensed.md
+++ tests/testthat/_snaps/parse-iupac-condensed.md
@@ -4,6 +4,6 @@
       print(glycan, verbose = TRUE)
     Output
       <glycan_structure[1]>
-      [1] Neu5Ac(a2-3)Gal(b1-4)[Fuc(a1-3)]GlcNAc(b1-6)[Neu5Ac(a2-3)Gal(b1-3)]GalNAc(?1-
+      [1] Neu5Ac(a2-3)Gal(b1-4)[Fuc(a1-3)]GlcNAc(b1-6)[Neu5Ac(a2-3)Gal(b1-3)]GalNAc(b1-
       # Unique structures: 1
 
--- tests/testthat/test-parse-iupac-condensed.R
+++ tests/testthat/test-parse-iupac-condensed.R
@@ -1,6 +1,6 @@
 test_that("IUPAC-condensed: some O-glycan", {
   skip_on_old_win()
-  to_parse <- "Neu5Ac(a2-3)Gal(b1-4)[Fuc(a1-3)]GlcNAc(b1-6)[Neu5Ac(a2-3)Gal(b1-3)]GalNAc"
+  to_parse <- "Neu5Ac(a2-3)Gal(b1-4)[Fuc(a1-3)]GlcNAc(b1-6)[Neu5Ac(a2-3)Gal(b1-3)]GalNAc(b1-"
   glycan <- parse_iupac_condensed(to_parse)
   expect_snapshot(print(glycan, verbose = TRUE))
 })
