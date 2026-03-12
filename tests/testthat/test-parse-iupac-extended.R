d11bf3444588d3e6be6168010b18390eb887b8ff
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2026-03-12 17:01:02 +0800

    test: expand plain text format tests

--- tests/testthat/test-parse-iupac-extended.R
+++ tests/testthat/test-parse-iupac-extended.R
@@ -23,8 +23,29 @@ test_that("plain text format is parsed correctly", {
     graph <- glyrepr::get_structure_graphs(structure)
     expect_equal(igraph::V(graph)$mono, mono)
   }
+  expect_anomer <- function(iupac_ext, anomer) {
+    structure <- parse_iupac_extended(iupac_ext)
+    graph <- glyrepr::get_structure_graphs(structure)
+    expect_equal(graph$anomer, anomer)
+  }
+  expect_linkage <- function(iupac_ext, linkage) {
+    structure <- parse_iupac_extended(iupac_ext)
+    graph <- glyrepr::get_structure_graphs(structure)
+    expect_equal(igraph::E(graph)$linkage, linkage)
+  }
+
+  # Monosaccharides
   expect_mono("alpha-D-Glcp-(1->", "Glc")
   expect_mono("beta-D-Glcp-(1->", "Glc")
+  expect_mono("alpha-D-GlcpNAc-(1->", "GlcNAc")
+
+  # Anomers
+  expect_anomer("alpha-D-Glcp-(1->", "a1")
+  expect_anomer("beta-D-Glcp-(1->", "b1")
+
+  # Linkages
+  expect_linkage("alpha-D-Galp-(1->4)-beta-D-Galp-(1->", "a1-4")
+  expect_linkage("beta-D-Galp-(1->4)-beta-D-Galp-(1->", "b1-4")
 })
 
 
